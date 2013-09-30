fc0ca612e79632ab435fb56544ea49579921dfe1
Tu Chu Quang <tucq88@gmail.com>
2013-09-30 19:56:45 +0700

    complete desktop layout. Remain responsive customize

--- html/index.php
+++ html/index.php
@@ -124,7 +124,7 @@
     </header>
 
     <div class="main">
-        <div class="service container">
+        <div class="service main-block container">
             <h1 class="block-title"><span>OUR SERVICES</span></h1>
             <div class="row">
                 <article class="col-md-4">
@@ -144,34 +144,33 @@
                 </article>
             </div>
         </div>
-        <div class="latest">
+        <div class="latest main-block">
             <h1 class="block-title"><span>LATEST POSTS</span></h1>
-            <div class="latest-content" style="margin: 0 auto;width: 1000px;overflow: auto">
+            <div class="latest-content container">
                 <?php for ($row = 1; $row <= 2; ++$row):?>
-                    <div style="width:2600px;">
+                    <div style="width: 5000px;">
                         <?php 
                             $totalCol = 0;
                             while ($totalCol < 12):
                                 $col = rand(1, 2);
                                 $totalCol += $col;
-                                $height = '200'; 
-                                if ($col == 2) {
-                                    $res = '400x200';
-                                    $width = '400';
-                                } elseif ($col == 1) {
-                                    $res = '200x200';
-                                    $width = $height;
+                                if ($totalCol > 12 && $col == 2) {
+                                    $col = 1;
                                 }
-                                
+                                $height = 240;
+                                $width = 356;
+                                $res = $width * $col . 'x' . $height;
                         ?>
-                            <article class="" style="max-width: <?php echo $width?>px; display: inline-block; float:left;">
+                            <article class="<?php echo ($col == 2) ? 'two' : 'one';?>">
                                 <img src="holder.js/<?php echo $res?>">
                                 <div class="description">
-                                    <p class="">
+                                    <time>AAAAAA</time>
+                                    <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+                                    <p>
                                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel adipiscing nisl. 
                                         Sed ultrices, elit vitae pulvinar tincidunt, nunc odio vestibulum eros, sit amet semper felis magna id diam
                                     </p>
-                                    <a href="#" class="read-more btn">Read More</a>
+                                    <a href="#" class="read-more btn btn-default">Read More</a>
                                 </div>
                             </article>
                         <?php endwhile;?>
@@ -179,45 +178,64 @@
                 <?php endfor;?>
             </div>
         </div>
-        <div class="popular">
+        <div class="popular main-block hide">
             <h1 class="block-title"><span>POPULAR POSTS</span></h1>
         </div>
-        <div class="customers">
-            <ul>
-                <li><a href="#"><img src="holder.js/128x128"></a></li>
-                <li><a href="#"><img src="holder.js/128x128"></a></li>
-                <li><a href="#"><img src="holder.js/128x128"></a></li>
-                <li><a href="#"><img src="holder.js/128x128"></a></li>
-                <li><a href="#"><img src="holder.js/128x128"></a></li>
-            </ul>
+        <div class="customers main-block container">
+            <div class="customers-content row">
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+                <div class="col-md-2"><a href="#"><img src="holder.js/128x128/#5E5E5F:#E7EBEE"></a></div>
+            </div>
         </div>
-        <div class="contact-info">
+        <div class="contact-info main-block container">
             <h1 class="block-title"><span>Contact Us</span></h1>
-            <div class="contact">
-                <a class="logo"><img src="holder.js/300x100"></a>
-                <ul>
-                    <li class="address">Lorem ipsum dolor sit amet, consectetur adipiscing elit</li>
-                    <li class="phone">xxx-xxx-xxxx</li>
-                    <li class="mail"><a href="mailto:contact@sample.domain">contact@sample.domain</a></li>
-                    <li class="website"><a href="wwwsampledomain.vn">wwwsampledomain.vn</a></li>
-                </ul>
-            </div>
-            <div class="map">
-                <img src="holder.js/400x400">
-            </div>
-            <div class="fanpage">
-                <img src="holder.js/400x400">
+            <div class="contact-info-container row">
+                <div class="contact col-md-4">
+                    <ul>
+                        <li><a class="logo"><img src="holder.js/300x100"></a></li>
+                        <li>
+                            <span class="glyphicon glyphicon-map-marker"></span>
+                            <p>702 Anh Minh Building, Hoang Cau Str, Dong Da Dist, Hanoi.</p>
+                        </li>
+                        <li class="phone">
+                            <span class="glyphicon glyphicon-phone-alt"></span>
+                            <p>xxx-xxx-xxxx</p>
+                        </li>
+                        <li class="mail">
+                            <span class="glyphicon glyphicon-envelope"></span>
+                            <p><a href="mailto:contact@sample.domain">contact@sample.domain</a></p>
+                        </li>
+                        <li class="website">
+                            <span class="glyphicon glyphicon-globe"></span> 
+                            <p><a href="wwwsampledomain.vn">wwwsampledomain.vn</a></p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="map col-md-4">
+                    <img src="holder.js/300x400">
+                </div>
+                <div class="fanpage col-md-4">
+                    <img src="holder.js/300x400" class="pull-right">
+                </div>
             </div>
         </div>
     </div>
 
     <footer>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nulla sapien, consectetur dictum arcu.<br>Praesent justo felis, volutpat in mi vitae</p>
-        <ul class="social-link">
-            <li><a href="#"><img src="holder.js/16x16"></a></li>
-            <li><a href="#"><img src="holder.js/16x16"></a></li>
-            <li><a href="#"><img src="holder.js/16x16"></a></li>
-        </ul>
+        <div class="footer-content container">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in nulla sapien, consectetur dictum arcu.<br>Praesent justo felis, volutpat in mi vitae</p>
+            <ul class="social-link nav nav-pills">
+                <li><a href="#"><img src="holder.js/16x16"></a></li>
+                <li><a href="#"><img src="holder.js/16x16"></a></li>
+                <li><a href="#"><img src="holder.js/16x16"></a></li>
+                <li><a href="#"><img src="holder.js/16x16"></a></li>
+                <li><a href="#"><img src="holder.js/16x16"></a></li>
+            </ul>
+        </div>
     </footer>
 
 
@@ -225,6 +243,29 @@
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/holder.js"></script>
+    <script src="assets/js/jquery.mousewheel.js"></script>
+    <script src="assets/js/jquery.nicescroll.min.js"></script>
     <script src="assets/js/custom.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+    	$(".latest-content").niceScroll({
+    		cursorcolor: "#c71c2e",
+            cursorwidth:"16px",
+//             cursorborder:"none",
+            cursorborderradius:"0px",
+            /* cursoropacitymin:"1", */
+            background:"#f0f3f4",
+            railpadding: {top:0,right:0,left:0,bottom:0}
+    	});
+        
+    	$(".latest-content").mousewheel(function(event, delta, deltaX, deltaY) {
+    		this.scrollLeft -= (delta * 30);
+    		if (delta < 0) {
+    		} else if (delta > 0) {
+    		}
+    		return false;
+	    });
+    });
+    </script>
 </body>
 </html>
