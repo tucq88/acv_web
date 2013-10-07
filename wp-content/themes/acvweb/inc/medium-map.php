968b40b8e24bbe9203a1be80b8fe24d79aef350d
Tiennv <vipnd2003@gmail.com>
2013-10-07 16:27:39 +0700

    Update style page contact us

--- wp-content/themes/acvweb/inc/medium-map.php
+++ wp-content/themes/acvweb/inc/medium-map.php
@@ -4,24 +4,51 @@
     <script>
         var map;
         function initialize() {
+          var myLatLng = new google.maps.LatLng(21.018225, 105.824132);
           var mapOptions = {
             zoom: 16,
-            center: new google.maps.LatLng(21.016497, 105.821950),
+            center: myLatLng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           };
           map = new google.maps.Map(document.getElementById('map-canvas'),
               mapOptions);
+
+          var marker = new google.maps.Marker({
+              position: myLatLng,
+              map: map,
+              title: 'Atmarkcafe Viet Nam'
+          });
         }
         
         google.maps.event.addDomListener(window, 'load', initialize);
     
     </script>
     <div class="container" style="background-color : #E7EBEF; padding: 0px 30px 40px 30px;">
+        <hr style="border-top: 1px solid #3a3937;">
+        <div class="row">
+            <div class="col-lg-11">
+                <div style="font-size: 18px; margin-bottom : 20px;">
+                    <span class="glyphicon glyphicon-map-marker" style="color : #c8162a; margin-right: 10px;"></span><?php _e('You can find us here', 'acvdev') ?>
+                </div>
+            </div>
+        </div>
         <div class="row">
             <div class="col-lg-8 col-md-8">
                 <div id="map-canvas" style="height : 400px;"></div>
             </div>
             <div class="col-lg-4 col-md-4">
+                <div style="font-size: 18px; margin-bottom : 20px;">
+                    <span class="glyphicon glyphicon-map-marker" style="color : #c8162a; margin-right: 10px;"></span><?php _e('Tang 7, Toa nha Anh Minh', 'acvdev') ?>
+                </div>
+                <div style="font-size: 18px; margin-bottom : 20px;">
+                    <span class="glyphicon glyphicon-earphone" style="color : #c8162a; margin-right: 10px;"></span><?php _e('043.734.9747', 'acvdev') ?>
+                </div>
+                <div style="font-size: 18px; margin-bottom : 20px;">
+                    <span class="glyphicon glyphicon-envelope" style="color : #c8162a; margin-right: 10px;"></span><?php _e('phamhung@atmarkcafe.org', 'acvdev') ?>
+                </div>
+                <div style="font-size: 18px; margin-bottom : 20px;">
+                    <span class="glyphicon glyphicon-globe" style="color : #c8162a; margin-right: 10px;"></span><?php _e('www.atmarkcafevn.org', 'acvdev') ?>
+                </div>
             </div>
         </div>
     </div>
