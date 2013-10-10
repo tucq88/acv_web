04ab84ffaaeda14dec0261c24b304d9329e12400
Tiennv <vipnd2003@gmail.com>
2013-10-10 11:40:21 +0700

    Update css search.php

--- wp-content/themes/acvweb/search.php
+++ wp-content/themes/acvweb/search.php
@@ -13,11 +13,14 @@
     font-size : 13px !important;
     text-align : center;
 }
-.searchtitle-content {
-    padding : 5px;
+.searchtitle-info {
+    padding : 5px 10px;
     font-size : 11px;
     color : #acb0b5;
     margin-top : 5px;
+}
+.searchtitle-content {
+    padding : 0px 10px;
 }
 .searchtitle-link {
     font-size : 22px;
@@ -60,20 +63,22 @@
             			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
             			    <div class="row">
             			        <div class="col-sm-3">
-            			            <div class="searchtitle-time searchtitle-content"><?php the_time('F jS, Y') ?></div>
-            			            <div class="searchtitle-content">
+            			            <div class="searchtitle-time searchtitle-info"><?php the_time('F jS, Y') ?></div>
+            			            <div class="searchtitle-info">
             			                <span class="glyphicon glyphicon-user"></span>  
             			                <i><?php the_author() ?></i>
         			                </div>
-            			            <div class="searchtitle-content">
+            			            <div class="searchtitle-info">
             			                <span class="glyphicon glyphicon-comment"></span>  
             			                <i><?php comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?></i>
         			                </div>
             			        </div>
             			        <div class="col-sm-9">
-            			            <h4><a href="<?php the_permalink() ?>" class="searchtitle-link"><span><?php the_title(); ?></span></a></h4>
-            			            <div>
-                    					<?php the_excerpt(); ?>
+            			            <div class="searchtitle-content">
+                			            <h4><a href="<?php the_permalink() ?>" class="searchtitle-link"><span><?php the_title(); ?></span></a></h4>
+                			            <div>
+                        					<?php the_excerpt(); ?>
+                        				</div>
                     				</div>
             			        </div>
             			    </div>
